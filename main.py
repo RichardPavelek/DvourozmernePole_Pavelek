e55809f653691a652ad067164024124205ca4133
RichardPavelek <98985@student.vitkovickastredni.cz>
2025-01-21 12:16:34 +0100

    add_row

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ array = [
     [14, 18, 12],
     [28, 26, 24]
 ]
-#výpis pole
+#Výpis pole
 print(array)
 
-#změna hodnoty v druhém řádku a druhém sloupci
+#Změna hodnoty v druhém řádku a druhém sloupci
 array[1][1] = 105
 
-#výpis pole
+#Výpis pole
+print(array)
+
+#Nový řádek 
+new_row = [100, 96, 43, 57]  
+array.append(new_row)
+
+# Přidání sloupce
+for i in range(len(array)):
+    array[i].append(17)
+
+# Výpis pole
 print(array)
